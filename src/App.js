a32dee1042fa8c51e3028686e4a1cb6e09532a8d
Humberto Vallejo <111450057+humbertovacu@users.noreply.github.com>
2023-02-11 14:22:09 +0100

    CreateJourney Component for testig and EditTags Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import CreateStep from './components/CreateStep/CreateStep';
 import CreateBlock from './components/CreateBlock/CreateBlock';
+import CreateJourney from './components/CreateJourney/CreateJourney'
 import {Routes, Route} from "react-router-dom"
 import { Link } from 'react-router-dom';
 import EditStep from './components/EditStep/EditStep';
@@ -17,6 +18,7 @@ function App() {
         <Route path={"/create-step"} element={<BlocksStepsPage/>} />
         <Route path={"/edit-step/:stepId"} element={<EditStep/>}/>
         <Route path={"/blocks"} element={<CreateBlock/>}/>
+        <Route path={"/journeys"} element={<CreateJourney/>}/>
       </Routes>
     </div>
   );
--- src/components/CreateJourney/CreateJourney.js
+++ src/components/CreateJourney/CreateJourney.js
@@ -1,22 +1,79 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import EditTags from "../EditTags/EditTags";
+const API_ROUTE = process.env.REACT_APP_SERVER_URL;
+const allTags = [];
 
 
 function CreateJourney(){
 
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [image, setImage] = useState('');
+    const [tag, setTag] = useState('');
+    const [tagArray, setTagArray] = useState([]);
+    const [isPublic, setIsPublic] = useState(false);
+    const [errorMessage, setErrorMessage] = useState(null);
+
+
+    const handleClick = () => {
+        if(tag){
+            allTags.push(tag);
+            setTagArray(allTags);
+            setTag('')
+        };
+    }
+
+    const handleUpload = (event) => {
+        
+        const uploadData = new FormData();
+        uploadData.append('imageUrl', event.target.files[0])
+        axios.post(`${API_ROUTE}/api/upload`, uploadData)
+            .then(response => setImage(response.data.imageUrl))
+            .catch(error => setErrorMessage(error.response.data.message));
+
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(title && description){
+        const userJourney = {title, description, image, tags: tagArray, isPublic};
+        axios.post(`${API_ROUTE}/api/journeys`, userJourney)
+            .then(response => console.log(response.data))
+        } else {
+            setErrorMessage('Please Add a Title and a Description to Your Journey')
+        }
+    }
+
     return(
         <div>
-            <form>
-                <label>
-                    <input type='text' name='title'/>
+            <form onSubmit={(event) => handleSubmit(event)}>
+                <label>Title:
+                    <input type='text' name='title' onChange={(event) => setTitle(event.target.value)}/>
                 </label>
-                <label>
-                    <input type='text' name='description'/>
+                <br/>
+                <label>Description:
+                    <input type='text' name='description' onChange={(event) => setDescription(event.target.value)}/>
                 </label>
-                <label>
-                    <input type='text' name='tags'/>
+                <br/>
+                <label>Image:
+                    <input type='file' name='imageUrl' onChange={(event) => handleUpload(event)}/>
                 </label>
-                <label>
-                    <input type='checkbox' name='isPublic'/>
+                <br/>
+
+                {tagArray && <EditTags tagArray={tagArray} allTags={allTags} setTagArray={setTagArray}/>}
+
+                <label>Tags:
+                    <input type='text' name='tags' onChange={(event) => setTag(event.target.value)}/>
                 </label>
+                <button type="button" onClick={handleClick}>Add tag</button>
+                <br/>
+                <label>Make Journey Public:
+                    <input type='checkbox' name='isPublic' onChange={(event) => setIsPublic(event.target.checked)}/>
+                </label>
+                <br/>
+                {errorMessage && <h2>{errorMessage}</h2>}
+                <button>Create Journey</button>
             </form>
         </div>
     )
