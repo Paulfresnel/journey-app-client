7e445fdbfb730bc829072192603dbae3a782f3e8
Paulfresnel <paul.fresnel1@gmail.com>
2023-02-21 19:19:55 +0100

    Public Journeys Page + Like feature functional
    Added bootstrap CSS cdns + icons cdn

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -12,13 +12,7 @@ function HomePage(){
         <div>
             
 
-    <Link to={"blocks/63e779d2a9978c010d5502dc/edit"}>
-      <button>Create a New Step</button>
-    </Link>
-
-    <Link to={"/blocks"}>
-      <button>Create a Block</button>
-    </Link>
+           
     
     <Link to={"/journeys"}>
       <button>Create a Journey</button>
